4b978d454ce57a1aa2565c927586244276ab31bf
pintug <pintug@nrifintech.com>
2021-06-26 17:35:21 +0530

    Added getData Method and Constants Class

---
+++ src/com/pintug/easylearning/Constants.java
@@ -0,0 +1,7 @@
+package com.pintug.easylearning;
+
+public class Constants {
+	
+	public final static String DATA_TYPE = "Integer";
+
+}
--- src/com/pintug/easylearning/GitDemo.java
+++ src/com/pintug/easylearning/GitDemo.java
@@ -2,6 +2,12 @@ package com.pintug.easylearning;
 
 public class GitDemo {
 	
+	public static void addData(String data) {
+		if(data.equals(Constants.DATA_TYPE)) {
+			System.out.println("Welcome");
+		}
+	}
+	
 	public static void getGreeting() {
 		System.out.println("Greeting from Git Demo");
 	}
