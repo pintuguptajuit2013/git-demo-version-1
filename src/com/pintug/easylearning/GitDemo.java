56a6b502a9ba59b719a8ec6678aeb4ee74282417
pintug <pintug@nrifintech.com>
2021-06-26 17:10:24 +0530

    Added the greeting method in GIT demo

--- src/com/pintug/easylearning/GitDemo.java
+++ src/com/pintug/easylearning/GitDemo.java
@@ -1,6 +1,10 @@
 package com.pintug.easylearning;
 
 public class GitDemo {
+	
+	public static void getGreeting() {
+		System.out.println("Greeting from Git Demo");
+	}
 	public static void main(String[] args) {
 		System.out.println("Happy GIT Learning");
 	}
